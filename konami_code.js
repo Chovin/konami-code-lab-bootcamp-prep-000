d050ccd994bef01729b588e139c8bc6375021537
Chovin Carlson <chovin@guam.net>
2018-08-26 01:48:37 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -23,9 +23,12 @@ function init() {
         next_key = 0
       }
       if (next_key === codes.length) {
-        alert('+30 Karma')
-        var karma = document.querySelector('#js--karma-points-count')
-        karma.innerHTML = parseInt(karma.innerHTML) + 30
+        // alert('+30 Karma')
+        // var karma = document.querySelector('#js--karma-points-count')
+        // karma.innerHTML = parseInt(karma.innerHTML) + 30
+        alert('+30 passes')
+        var passes = document.querySelector('.passes em')
+        passes.innerHTML = parseInt(passes.innerHTML) + 30
         next_key = 0
       }
     })
