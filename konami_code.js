64f3ba8fbbc408381b1204efa35138ba26048855
Chovin Carlson <chovin@guam.net>
2018-08-26 01:35:39 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -13,17 +13,21 @@ const codes = [
 
 function init() {
   var mext_key = 0
-  document.body.addEventListener('keydown', function(e) {
-    if (e.key === codes[next_key]) {
-      next_key += 1
-    } else {
-      next_key = 0
+  document.body.addEventListener('keydown', 
+  function() {
+    var mext_key = 0
+    function(e) {
+      if (e.key === codes[next_key]) {
+        next_key += 1
+      } else {
+        next_key = 0
+      }
+      if (next_key === codes.length) {
+        alert('+30 Karma')
+        var karma = document.querySelector('#js--karma-points-count')
+        karma.innerHTML = parseInt(karma.innerHTML) + 30
+        next_key = 0
+      }
     }
-    if (next_key === codes.length) {
-      alert('+30 Karma')
-      var karma = document.querySelector('#js--karma-points-count')
-      karma.innerHTML = parseInt(karma.innerHTML) + 30
-      next_key = 0
-    }
-  })
+  }())
 }
